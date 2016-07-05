9f95ad0fb83a304a17b1cd0373c323e1f3029e8c
Jeremy Josey <webjeremy@gmail.com>
2016-07-05 12:20:17 -0300

    Updated shortcode

--- src/jhj-crtc/classes/shortcode.php
+++ src/jhj-crtc/classes/shortcode.php
@@ -2,6 +2,8 @@
 
 class JhjCrtc_Shortcode{
 
+	private $fileurl;
+
 	function __construct() {
 		$this->add_shortcodes();
 	}
@@ -11,6 +13,41 @@ class JhjCrtc_Shortcode{
 	}
 
 	function get_crtc_list() {
-		return 'the list will go here';
+		$crtc_str = '';
+		$crtc_terms = get_terms( 'Category' );
+
+		foreach ( $crtc_terms as $crtc_term ) {
+		    $crtc_term_query = new WP_Query( array(
+		        'post_type' => 'crtc',
+		        'posts_per_page' => -1,
+		        'tax_query' => array(
+		            array(
+		                'taxonomy' => 'category',
+		                'field' => 'slug',
+		                'terms' => array( $crtc_term->slug ),
+		                'operator' => 'IN'
+		            )
+		        )
+		    ) );
+		    //ensure category before appending
+		    if ( 'Uncategorized' !== $crtc_term->name ) {
+
+				$crtc_str .= '<h2>'. $crtc_term->name . '</h2>';
+				$crtc_str .= '<ul>';
+				if ( $crtc_term_query->have_posts() ) : while ( $crtc_term_query->have_posts() ) : $crtc_term_query->the_post();
+						// get the url for the file
+						$this->fileurl = get_post_meta( get_the_ID(), 'jhj_ctrc_file_url', true );
+						//append to string
+						$crtc_str .= '<li><a href="'. $this->fileurl.'" target="_blank">' . get_the_title() . '</a></li>';
+				endwhile; endif;
+
+				$crtc_str .= '</ul>';
+			}
+
+			// Reset things
+			$member_group_query = null;
+			wp_reset_postdata();
+		}
+		return $crtc_str;
 	}
 }
